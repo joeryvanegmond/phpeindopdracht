3f5abd4e3cf004010016f69f36a04057b5dc50b0
Joery van Egmond <joeryvanegmond@hotmail.com>
2020-03-28 22:28:39 +0100

    dashboard 1/2 done

--- eindopdracht/app/Course.php
+++ eindopdracht/app/Course.php
@@ -23,4 +23,52 @@ class Course extends Model
     {
         return $this -> hasMany('\App\Test','course_id', 'id');
     }
+
+    public function totalPointsInBlock($period)
+    {
+        $totalPoints = 0;
+        foreach (Course::all()->where('periode', '=', $period) as $course) {
+            $totalPoints =+ $course->studiepunten;
+        }
+        return $totalPoints;
+    }
+
+    public function earnedPoints($period)
+    {
+        $earnedPoints = 0;
+        foreach (Course::all()->where('periode', '=', $period) as $course) {
+            foreach($course->tests()->get() as $test)
+            {
+                if ($test->cijfer > 5)
+                {
+                    $earnedPoints =+ $course->studiepunten;
+                }
+            }
+        }
+        return $earnedPoints;
+    }
+
+    public function totalPoints()
+    {
+        $totalPoints = 0;
+        foreach (Course::all() as $course) {
+            $totalPoints =+ $course->studiepunten;
+        }
+        return $totalPoints;
+    }
+
+    public function totalEarnedPoints()
+    {
+        $totalPoints = 0;
+        foreach (Course::all() as $course) {
+            foreach($course->tests()->get() as $test)
+            {
+                if ($test->cijfer > 5)
+                {
+                    $totalPoints =+ $course->studiepunten;
+                }
+            }
+        }
+        return $totalPoints;
+    }
 }
--- eindopdracht/resources/views/dashboard/index.blade.php
+++ eindopdracht/resources/views/dashboard/index.blade.php
@@ -1,25 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row d-flex justify-content-center mt-5">
+    <div class="row d-flex mt-5">
+                <div class="h1">Studievoortgang</div>
         <div class="col-lg-12">
+            <div class="col-12 d-flex justify-content-center">
+                <progress class="progress-bar w-100 mt-4 mb-4" style="height: 40px;" value="{{$modules->first()->totalEarnedPoints()}}" max="{{$modules->first()->totalPoints()}}"></progress>
+                <span class="position-absolute text-white" style="margin-top: 31px;">{{$modules->first()->totalEarnedPoints()}} / {{$modules->first()->totalPoints()}}</span>
+            </div>
 
             @foreach($periods as $periode)
-                <div class="col-12 m-3">
-                    <div class="card">
+                <div class="col-12 mb-4">
+                    <div class="card shadow">
                         <div class="card-header">
                             Leerjaar {{$periode}}
                         </div>
                         <div class="card-body d-flex">
                             @foreach($blocks as $block)
-                                <div class="col-3">
-                                    <div class="h5">Blok {{$blockCounter++}}</div>
+                                <div class="col-3 border-left border-right">
+                                    <div class="h5 font-weight-bold">Blok {{$blockCounter++}}</div>
+                                        <progress class="progress-bar w-100" value="{{$modules->first()->earnedPoints($blockCounter - 1)}}" max="{{$modules->first()->totalPointsInBlock($blockCounter - 1)}}"></progress>
                                         <div class="d-flex flex-column">
                                             <span class="font-weight-bold">Modules:</span>
-                                            @foreach($modules->where('periode', '=', $blockCounter - 1) as $module)
-                                                <span>{{$module->name}}</span>
-                                            @endforeach
-                                            //get totalstudypoints
+                                            <span class="border-bottom d-flex flex-column">
+
+                                                @foreach($modules->where('periode', '=', $blockCounter - 1) as $module)
+                                                    @if($module->tests()->whereYear('version', '=', now()->year)->first() != null && $module->tests()->whereYear('version', '=', now()->year)->first()->cijfer != null)
+                                                        @if($module->tests()->whereYear('version', '=', now()->year)->first()->cijfer > 5)
+                                                                <span>{{$module->name}} <span class="pull-right text-success font-weight-bold">Cijfer: {{$module->tests()->whereYear('version', '=', now()->year)->first()->cijfer}}</span></span>
+                                                            @else
+                                                                <span>{{$module->name}} <span class="pull-right text-danger font-weight-bold">{{$module->tests()->whereYear('version', '=', now()->year)->first()->cijfer}}</span></span>
+                                                            @endif
+                                                        @else
+                                                        <span>{{$module->name}}</span>
+                                                        @endif
+                                                @endforeach
+                                            </span>
+                                                    <span>
+
+                                                        <strong>Punten:</strong>
+                                                        @if($modules->first()->earnedPoints($blockCounter - 1) != $modules->first()->totalPointsInBlock($blockCounter - 1) || $modules->first()->totalPointsInBlock($blockCounter - 1) == 0)
+                                                                <strong class="text-danger"> {{$modules->first()->earnedPoints($blockCounter - 1)}} / {{$modules->first()->totalPointsInBlock($blockCounter - 1)}}</strong>
+                                                            @else
+                                                                <strong class="text-success"> {{$modules->first()->earnedPoints($blockCounter - 1)}} / {{$modules->first()->totalPointsInBlock($blockCounter - 1)}}</strong>
+                                                        @endif
+                                                    </span>
                                         </div>
                                 </div>
                             @endforeach
